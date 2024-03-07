0d11c1ade2d1690097871fe1c29a3e9cb271df07
Руслан Бражников <ruslan.1995mai@gmail.com>
2024-03-07 17:33:28 +0500

    finish

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-$(function () {
-    $("[data-scroll]").on("click", function (event) {
+$(function() {
+    $("[data-scroll]").on("click", function(event){
         event.preventDefault();
 
         blockId = $(this).data('scroll');
         blockOffset = $(blockId).offset().top;
 
         $("html, body").animate({
-            scrollTop: blockOffset - 50
-        }, 600)
+            scrollTop: blockOffset - 50    
+        }, 700);
+
     });
 
-    $("[data-modal]").on("click", function (event) {
+    $("[data-modal]").on("click", function(event){
         event.preventDefault();
 
         $(modal_resume).addClass('show');
+
     });
 
-    $(modal_resume).on("click", function () {
+    $(modal_resume).on("click", function() {
         $(modal_resume).removeClass('show');
     });
-
 });
